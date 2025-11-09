801997d6b8933be19150a4d98559cb365242c7ea
Alexandra <alexismessing@gmail.com>
2025-11-09 23:30:13 +0300

    temp

---
+++ back/migration-api/src/WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Dto;
+using Application.UseCases.Commands.Seeding.Dtos;
+using Application.UseCases.Commands.Update.Customer;
+using Application.UseCases.Queries.Customer.Get.All;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomerController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CustomerController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("Get/All")]
+    public async Task<ActionResult<PaginatedResult<CustomerDto>>> GetCustomers([FromQuery] GetPaginatedCustomersQuery query)
+    {
+        var result = await _mediator.Send(query);
+        return result;
+    }
+
+    [HttpPost("Update")]
+    public async Task<ActionResult<Result<CustomerDto>>> UpdateCustomer([FromBody] UpdateCustomerCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return result;
+    }
+}
